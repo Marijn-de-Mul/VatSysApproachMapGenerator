6a1b7eb602c55ca8caa242beee11736e0e02ad9f
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2023-12-14 21:35:18 +0100

    Base

--- main.py
+++ main.py
@@ -23,6 +23,31 @@ def format_position(lat, lon):
     lon_str = f"{lon_sign}{lon:03.4f}".zfill(9)
     return f"{lat_str}{lon_str}"
 
+def opposite_runway_number(runway_number):
+    runway_number_base = runway_number.rstrip('LRC')
+    runway_number_base = int(runway_number_base)
+
+    if runway_number_base <= 18:
+        opposite_runway_number_base = runway_number_base + 18
+    else:
+        opposite_runway_number_base = runway_number_base - 18
+
+    if 'L' in runway_number:
+        opposite_runway_number_suffix = 'R'
+    elif 'R' in runway_number:
+        opposite_runway_number_suffix = 'L'
+    else:
+        opposite_runway_number_suffix = ''
+
+    return f"{opposite_runway_number_base:02d}{opposite_runway_number_suffix}"
+
+def get_opposite_heading(heading):
+    heading = int(heading)
+    if heading < 180:
+        return heading + 180
+    else:
+        return heading - 180
+
 for i, line in enumerate(lines):
     line_parts = line.split(',')
     if line_parts[0] == 'A' and line_parts[1] == icao:
@@ -51,28 +76,40 @@ for i, line in enumerate(lines):
             map_elem.set("Priority", "3")
             map_elem.set("Center", airport_coords)
 
-            runway_elem = ET.SubElement(map_elem, "Runway")
-            runway_elem.set("Name", runway_number)
-
             for r_line in runway_lines:
-                r_number = r_line.split(',')[1]
-                lat, lon = map(float, r_line.split(',')[8:10])
+                r_parts = r_line.split(',')
+                r_number = r_parts[1]
+                lat, lon = map(float, r_parts[8:10])
                 r_coords = format_position(lat, lon)
-
-                threshold_elem = ET.SubElement(runway_elem, "Threshold")
-                threshold_elem.set("Name", r_number)
-                threshold_elem.set("Position", r_coords)
+                r_heading = r_parts[2]  
 
                 if r_number.rstrip('LRC') == runway_number:
-                    threshold_elem.set("ExtendedCentrelineTrack", "245.38")
-                    threshold_elem.set("ExtendedCentrelineLength", "12")
-                    threshold_elem.set("ExtendedCentrelineTickInterval", "1")
+                    runway_elem = ET.SubElement(map_elem, "Runway")
+                    runway_elem.set("Name", r_number)
 
-            # Parse SIDs
+                    threshold_elem1 = ET.SubElement(runway_elem, "Threshold")
+                    threshold_elem1.set("Name", r_number)
+                    threshold_elem1.set("Position", r_coords)
+                    opposite_r_heading = get_opposite_heading(r_heading)  
+                    threshold_elem1.set("ExtendedCentrelineTrack=", str(opposite_r_heading))  
+
+                    opposite_r_number = opposite_runway_number(r_number)
+                    opposite_r_coords = ''
+                    for opp_r_line in runway_lines:
+                        opp_r_parts = opp_r_line.split(',')
+                        opp_r_number = opp_r_parts[1]
+                        if opp_r_number == opposite_r_number:
+                            lat, lon = map(float, opp_r_parts[8:10])
+                            opposite_r_coords = format_position(lat, lon)
+                            break
+
+                    threshold_elem2 = ET.SubElement(runway_elem, "Threshold")
+                    threshold_elem2.set("Name", opposite_r_number)
+                    threshold_elem2.set("Position", opposite_r_coords)
+                    
             with open(f'Navdata/Proc/{icao}.txt', 'r') as f:
                 sid_lines = f.readlines()
 
-            # Create a set to store waypoints that have already been added
             added_waypoints = set()
 
             for sid_line in sid_lines:
@@ -80,21 +117,20 @@ for i, line in enumerate(lines):
                 if sid_parts[0] == 'SID' and sid_parts[2].rstrip('LRC') == runway_number:
                     sid_name = sid_parts[1]
                     comment = ET.Comment(f'SID: {sid_name}, Runway: {runway_number}')
-                    map_elem.append(comment)  # Add the comment to the 'Map' element
+                    map_elem.append(comment) 
                     line_elem = ET.SubElement(map_elem, "Line")
                     line_elem.set("Pattern", "Dotted")
-                    line_elem.text = ''  # Initialize line_elem.text to an empty string
+                    line_elem.text = '' 
 
                     waypoints = sid_lines[sid_lines.index(sid_line)+1:]
                     for waypoint in waypoints:
-                        if waypoint.startswith('SID'):  # Stop when we reach the next 'SID' line
+                        if waypoint.startswith('SID'):  
                             break
-                        if waypoint.startswith(('VA', 'DF', 'TF', 'CF')):  # Only add the line as a waypoint if it starts with a waypoint code
+                        if waypoint.startswith(('VA', 'DF', 'TF', 'CF')):  
                             waypoint_parts = waypoint.split(',')
-                            waypoint_name = waypoint_parts[1]  # The waypoint name is the first item in the line
+                            waypoint_name = waypoint_parts[1]  
                             line_elem.text += waypoint_name + '/'
 
-                    # Remove the trailing '/' from the last waypoint
                     if line_elem.text.endswith('/'):
                         line_elem.text = line_elem.text[:-1]
                                 
